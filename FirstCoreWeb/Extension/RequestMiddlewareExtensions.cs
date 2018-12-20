476a794541d7cd338b3acf753239bab617f5ae0b
sachem1 <jieqqq678@163.com>
2018-12-21 07:39:05 +0800

    添加测试webcore middleware

---
+++ FirstCoreWeb/Extension/RequestMiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FirstCoreWeb.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWeb.Extension
+{
+    public static class RequestMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestExceptionMiddle(this IApplicationBuilder builder)
+        {
+
+            
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ FirstCoreWeb/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWeb.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate request)
+        {
+            _next = request;
+        }
+        public Task Invoke(HttpContext context)
+        {
+            if (context.Request.Host.Value.Contains("localhost"))
+            {
+                context.Response.Cookies.Append("auth", "123456789");
+                
+            }
+            return _next.Invoke(context);
+        }
+    }
+}
